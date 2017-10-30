166084cd593ce1471ea8c5335f6b748fcde95145
Ankit Kapoor <ankit.kapoor@nagarro.com>
2017-10-30 23:45:45 +0530

    Adding reuse strategy

--- src/app/custom-reuse-strategy.ts
+++ src/app/custom-reuse-strategy.ts
@@ -2,28 +2,33 @@ import { RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle } from 
 
 export class CustomReuseStrategy implements RouteReuseStrategy {
 
-  private handlers: {[key: string]: DetachedRouteHandle} = {};
+    private handlers: { [key: string]: DetachedRouteHandle } = {};
 
 
-  shouldDetach(route: ActivatedRouteSnapshot): boolean {
-    return true;
-  }
+    shouldDetach(route: ActivatedRouteSnapshot): boolean {
+        console.log('should detach');
+        return true;
+    }
 
-  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-    this.handlers[route.url.join('/') || route.parent.url.join('/')] = handle;
-  }
+    store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
+        console.log('store');
+        this.handlers[route.url.join('/') || route.parent.url.join('/')] = handle;
+    }
 
-  shouldAttach(route: ActivatedRouteSnapshot): boolean {
-      const url: string = route.url.join('/') || route.parent.url.join('/');
-    return !!this.handlers[url];
-  }
+    shouldAttach(route: ActivatedRouteSnapshot): boolean {
+        console.log('shouldAttach');
+        const url: string = route.url.join('/') || route.parent.url.join('/');
+        return !!this.handlers[url];
+    }
 
-  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-    return this.handlers[route.url.join('/') || route.parent.url.join('/')];
-  }
+    retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
+        console.log('retrieve');
+        return this.handlers[route.url.join('/') || route.parent.url.join('/')];
+    }
 
-  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-    return future.routeConfig === curr.routeConfig;
-  }
+    shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+        console.log('shouldReuseRoute');
+        return future.routeConfig === curr.routeConfig;
+    }
 
 }
